53b31e5e75ebb5719846db5408c08626bc74f421
keerthanalalam2022 <keerthanalalam2022@gmail.com>
2026-06-17 11:05:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+function login() {
+    window.location.href = "dashboard.html";
+}
+
 function searchSubjects() {
+
+    let input = document.getElementById("searchBox").value.toLowerCase();
+
+    let subjects = document.querySelectorAll("#subjects a");
+
+    subjects.forEach(function(subject) {
+
+        let text = subject.textContent.toLowerCase();
+
+        if (text.includes(input)) {
+            subject.style.display = "inline";
+        } else {
+            subject.style.display = "none";
+        }
+
+    });
+}function searchSubjects() {
 
     let input = document.getElementById("searchBox").value.toLowerCase();
 
